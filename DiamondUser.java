2831ed67e183357bbbb8bcd1602a8f0885f7b164
Renee Gula <reneegula0218@gmail.com>
2024-05-12 16:00:10 +0800

    new1

---
+++ DiamondUser.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class DiamondUser {
+   public static void main(String args[]) {
+      Scanner reader = new Scanner(System.in);
+
+      System.out.print("Enter a number: ");
+      int n = reader.nextInt();
+
+      for (int i = 0; i < n; i++) {
+         for (int j = 0; j <= i; j++) { // executing
+            System.out.print("* ");
+         }
+         System.out.println("");
+      }
+
+      for (int i = n; i > 0; i--) {
+         for (int j = i - 1; j > 0; j--) { // executing
+            System.out.print("* ");
+         }
+         System.out.println("");
+      }
+   }
+}
